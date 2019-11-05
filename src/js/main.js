e53e907fc019847576666e167b609d99b735ccf5
feyaq13 <feyaq13@gmail.com>
2019-11-05 22:50:08 +0300

    правит поведение стилей

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,16 @@
 const $ = require('jquery')
 
-if (window.matchMedia('max-width: 768px')) {
-  init()
-}
+$(window).on('load resize', function () {
+  if (window.matchMedia('(max-width: 968px)').matches) {
+    document.querySelector('.city:first-child').addEventListener('click', showFullNavigation)
+  } else if (window.matchMedia('(min-width: 969px)').matches) {
+    document.querySelector('.city:first-child').removeEventListener('click', showFullNavigation)
+  }
 
-function init () {
-  $('.city:first-child').click(showFullNavigation)
-}
+  document.querySelector('.city:nth-child(2)').style.display = ''
+  document.querySelector('.city:nth-child(3)').style.display = ''
+})
 
 function showFullNavigation (e) {
-  if (e.target == $('.city')[0]) {
-    $('.city:not(:first-child)').toggle(500)
-  }
+  $('.city:not(:first-child)').toggle(500)
 }
